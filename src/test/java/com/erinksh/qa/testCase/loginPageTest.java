82af79f887a38b55fda72525cbb112e88158e674
erinksh <rinkush.sharda@ericsson.com>
2021-02-19 08:37:45 +0530

    Initial

--- src/test/java/com/erinksh/qa/testCase/loginPageTest.java
+++ src/test/java/com/erinksh/qa/testCase/loginPageTest.java
@@ -3,22 +3,17 @@ package com.erinksh.qa.testCase;
 import com.erinksh.qa.base.TestBase;
 import com.erinksh.qa.pages.loginPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
-
-
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+
+
 
 public class loginPageTest extends TestBase {
 
@@ -36,29 +31,29 @@ public class loginPageTest extends TestBase {
         FileInputStream inputSream = new FileInputStream(excel);
 
 
-        HSSFWorkbook wb = new HSSFWorkbook(inputSream);
-        HSSFSheet sheet = wb.getSheetAt(0);
+        XSSFWorkbook wb = new XSSFWorkbook(inputSream);
+        XSSFSheet sheet = wb.getSheetAt(0);
 
 
         int rows = sheet.getLastRowNum();
         int column = sheet.getRow(1).getLastCellNum();
 
          for(int r=0;r<rows;r++){
-             HSSFRow row = sheet.getRow(r);
+             XSSFRow row = sheet.getRow(r);
 
                  for(int c=0;c<column;c++){
 
-                    HSSFCell cell = row.getCell(c);
+                    XSSFCell cell = row.getCell(c);
 
                     switch(cell.getCellType()){
-
-                        case STRING:
-                            System.out.print(cell.getStringCellValue());
+                        case XSSFCell.CELL_TYPE_NUMERIC:
+                            System.out.print(cell.getNumericCellValue());
                             break;
-                        case NUMERIC:
-                            System.out.println(cell.getNumericCellValue());
+                        case XSSFCell.CELL_TYPE_STRING:
+                            System.out.println(cell.getStringCellValue());
                             break;
-
+                        default:
+                            throw new IllegalStateException("Unexpected value: " + cell.getCellType());
                     }
 
 
